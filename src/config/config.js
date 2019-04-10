1a8aa5afbbd6195ecb5f8bd3a50a4b390ba6cdd5
Brayan Giraldo <brayan.giraldo@yuxiglobal.com>
2019-04-10 12:13:36 -0500

    Go ahead

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,12 @@
 process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'local';
-process.env.URLDB = 'mongodb://localhost:27017/plataforma';
+
+let urlDB
+if (process.env.NODE_ENV === 'local'){
+	urlDB = 'mongodb://localhost:27017/plataforma';
+}
+else {
+	urlDB = 'mongodb+srv://bgiraldox:mysecretpass@nodejsbgiraldocourse-b49pj.mongodb.net/plataforma?retryWrites=true'
+}
+
+process.env.URLDB = urlDB
